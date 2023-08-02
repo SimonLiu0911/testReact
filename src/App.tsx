2d592bd8052bc99b24d30344dace67dd001e30ee
Simon <milkjhang@hotmail.com>
2023-08-02 15:08:11 +0800

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { DatePicker } from "antd";
-
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/layout/Layout';
+import { Login } from './pages/login/Login';
+import { Admin } from './pages/admin/Admin';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <DatePicker />
-      </header>
+      <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path='admin' element={<Admin />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/admin/Admin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+/**
+ * 管理後台的路由組件
+ */
+export const Admin = () => {
+  return (
+    <div className="admin">
+      Admin
+    </div>
+  );
+}
